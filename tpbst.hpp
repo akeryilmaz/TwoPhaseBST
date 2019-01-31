17af4db06c64afd972616efab0a745f89bd15abe
Buğra Aker Yılmaz <akerylm@gmail.com>
2019-01-31 22:45:47 +0200

    Add files via upload

---
+++ tpbst.hpp
@@ -0,0 +1,122 @@
+#ifndef TWOPHASEBST_H
+#define TWOPHASEBST_H
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <list>
+// do not add any other library.
+// modify parts as directed by assignment text and comments here.
+
+template <class T>
+class TwoPhaseBST {
+private: //do not change
+    struct SecondaryNode {
+        std::string key;
+        T data;
+        SecondaryNode *left;
+        SecondaryNode *right;
+
+        SecondaryNode(const std::string &k, const T &d, SecondaryNode *l, SecondaryNode *r);
+    };
+
+    struct PrimaryNode {
+        std::string key;
+        PrimaryNode *left;
+        PrimaryNode *right;
+        SecondaryNode *rootSecondaryNode;
+
+        PrimaryNode(const std::string &k, PrimaryNode *l, PrimaryNode *r, SecondaryNode *rsn);
+    };
+
+public: // do not change.
+    TwoPhaseBST();
+    ~TwoPhaseBST();
+
+    TwoPhaseBST &insert(const std::string &primaryKey, const std::string &secondaryKey, const T &data);
+    TwoPhaseBST &remove(const std::string &primaryKey, const std::string &secondaryKey);
+    TwoPhaseBST &print(const std::string &primaryKey = "", const std::string &secondaryKey = "");
+    T *find(const std::string &primaryKey, const std::string &secondaryKey);
+
+private: // you may add your own utility member functions here.
+    void destructNode(PrimaryNode * &root);
+    void destructNode(SecondaryNode * &root);
+
+private: // do not change.
+    PrimaryNode *root; //designated root.
+
+    // do not provide an implementation. TwoPhaseBST's are not copiable.
+    TwoPhaseBST(const TwoPhaseBST &);
+    const TwoPhaseBST &operator=(const TwoPhaseBST &);
+};
+
+template <class T>
+TwoPhaseBST<T>::SecondaryNode::SecondaryNode(const std::string &k, const T &d, SecondaryNode *l, SecondaryNode *r)
+        : key(k), data(d), left(l), right(r) {}
+
+template <class T>
+TwoPhaseBST<T>::PrimaryNode::PrimaryNode(const std::string &k, PrimaryNode *l, PrimaryNode *r, SecondaryNode *rsn)
+        : key(k), left(l), right(r), rootSecondaryNode(rsn) {}
+
+template <class T>
+TwoPhaseBST<T>::TwoPhaseBST() : root(NULL) {}
+
+template <class T>
+TwoPhaseBST<T>::~TwoPhaseBST() {
+    destructNode(root);
+}
+
+template <class T>
+TwoPhaseBST<T> &
+TwoPhaseBST<T>::insert(const std::string &primaryKey, const std::string &secondaryKey, const T &data) {
+    /* IMPLEMENT THIS */
+}
+
+template <class T>
+TwoPhaseBST<T> &
+TwoPhaseBST<T>::remove(const std::string &primaryKey, const std::string &secondaryKey) {
+    /* IMPLEMENT THIS */
+}
+
+template <class T>
+TwoPhaseBST<T> &TwoPhaseBST<T>::print(const std::string &primaryKey, const std::string &secondaryKey) {
+    /* IMPLEMENT THIS */
+}
+
+template <class T>
+T *TwoPhaseBST<T>::find(const std::string &primaryKey, const std::string &secondaryKey) {
+    /* IMPLEMENT THIS */
+}
+
+template <class T>
+void TwoPhaseBST<T>::destructNode(TwoPhaseBST::PrimaryNode * &root)
+{
+    if (root == NULL)
+        return;
+
+    destructNode(root->left);
+    destructNode(root->right);
+
+    destructNode(root->rootSecondaryNode);
+
+    delete root;
+
+    root = NULL;
+}
+
+template <class T>
+void TwoPhaseBST<T>::destructNode(TwoPhaseBST::SecondaryNode * &root)
+{
+    if (root == NULL)
+        return;
+
+    destructNode(root->left);
+    destructNode(root->right);
+
+    delete root;
+
+    root = NULL;
+}
+
+#endif //TWOPHASEBST_H
+
